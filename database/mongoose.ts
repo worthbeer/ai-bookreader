227339854fc3a34f842bf5fae8f9c7c6a7c2e419
worthbeer <67826062+worthbeer@users.noreply.github.com>
2026-03-08 13:35:07 -0400

    config error

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -2,8 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error('Please define the MONGODB_URI environment variable');
-
 declare global {
     var mongooseCache: {
         conn: typeof mongoose | null
@@ -14,6 +12,10 @@ declare global {
 const cached = global.mongooseCache || (global.mongooseCache = { conn: null, promise: null });
 
 export const connectToDatabase = async () => {
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable');
+    }
+
     if (cached.conn) return cached.conn;
 
     if (!cached.promise) {
